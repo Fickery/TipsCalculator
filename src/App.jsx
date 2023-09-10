f371ac6a33b469849b01f09325e24af9e0fcab54
Fickery <fickery901@gmail.com>
2023-09-10 21:20:05 +0800

    feat: Overall changes - JS,FUNC,LAYOUT

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,8 @@ function App() {
 
   return (
     <div id="content" className="main__cont">
-<<<<<<< HEAD
       <Left data={data} /> {/* Pass data as a prop */}
       <Right data={data} />
-=======
-      <Left />
-      <Right />
->>>>>>> ba53136728b80820a9d11a18589afc289f57b7b2
     </div>
   );
 }
